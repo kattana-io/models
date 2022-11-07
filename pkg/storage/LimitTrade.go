8821a0f8d353ba4233be8e1fa02b5eaf3df2543c
Andrii Prokhorovych <prohorovychua@gmail.com>
2022-11-07 11:02:26 +0200

    feat: add LimitTrade model, change LimitOrder model

---
+++ pkg/storage/LimitTrade.go
@@ -0,0 +1,19 @@
+package models
+
+type LimitTrade struct {
+	ID            uint   `gorm:"primaryKey" json:"id"`
+	OrderId       uint   `gorm:"column:order_id;type:int64" json:"order_id"`
+	Status        string `gorm:"column:status;type:VARCHAR;" json:"status"`
+	AmountIn      string `gorm:"column:amount_in;type:VARCHAR;" json:"amount_in"`
+	AmountOut     string `gorm:"column:amount_out;type:VARCHAR;" json:"amount_out"`
+	GasPrice      int64  `gorm:"column:gas_price;type:int64;" json:"gas_price"`
+	GasValue      int64  `gorm:"column:gas_value;type:int64;" json:"gas_value"`
+	TriggerTx     string `gorm:"column:trigger_tx;type:VARCHAR;" json:"trigger_tx"`
+	ExecutionTx   string `gorm:"column:execution_tx;type:VARCHAR;" json:"execution_tx"`
+	FailureReason string `gorm:"column:failure_reason;type:VARCHAR;" json:"failure_reason"`
+}
+
+// TableName overrides the table name used by User to `profiles`
+func (LimitTrade) TableName() string {
+	return "limit_trades"
+}
