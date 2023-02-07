4656b9307fba33a79ea0a86484002b5b42ccb22e
Aleksandr Kolesnik <kolesnikalex77@gmail.com>
2023-02-07 14:28:51 +0200

    fix: added field Amount to Holder struct

--- pkg/storage/Holder.go
+++ pkg/storage/Holder.go
@@ -1,8 +1,9 @@
 package models
 
 type Holder struct {
-	Token string `json:"token"`
-	From  string `json:"from"`
-	To    string `json:"to"`
-	Tx    string `json:"tx"`
+	Token  string `json:"token"`
+	From   string `json:"from"`
+	To     string `json:"to"`
+	Tx     string `json:"tx"`
+	Amount int64  `json:"amount,omitempty"`
 }
