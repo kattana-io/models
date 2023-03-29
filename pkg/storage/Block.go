be94dc2d7dc243a7b3a096222ecd022abcfe774e
Andrii Prokhorovych <prohorovychua@gmail.com>
2023-03-29 13:37:36 +0300

    feat: add block trace

--- pkg/storage/Block.go
+++ pkg/storage/Block.go
@@ -11,4 +11,11 @@ type Block struct {
 	Node      string   `json:"node"`
 	Notify    bool     `json:"notify"`
 	Timestamp uint64   `json:"timestamp"`
+
+	// block trace
+	DiscoveredAt       int64 `json:"discovered_at"`
+	ReceivedInParserAt int64 `json:"received_in_parser_at"`
+	ParsedAt           int64 `json:"parsed_at"`
+	ConsumedAt         int64 `json:"consumed_at"`
+	LiveSendAt         int64 `json:"live_send_at"`
 }
