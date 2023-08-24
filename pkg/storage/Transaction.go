2b97d6ad3dafab7c98d6c05a1f6c2ad73fdcbad5
Aleksandr Kolesnik <kolesnikalex77@gmail.com>
2023-08-24 14:10:02 +0300

    fix: changed Transaction fields type

--- pkg/storage/Transaction.go
+++ pkg/storage/Transaction.go
@@ -1,34 +1,36 @@
 package models
 
 import (
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/kattana-io/models/pkg-bin/storage"
 	"google.golang.org/protobuf/proto"
+	"math/big"
 )
 
 type Transaction struct {
-	From  string `json:"from"`
-	Hash  string `json:"hash"`
-	Input string `json:"input"`
-	To    string `json:"to"`
-	Value string `json:"value"`
+	From  common.Address `json:"from"`
+	Hash  common.Hash    `json:"hash"`
+	Input []byte         `json:"input"`
+	To    common.Address `json:"to"`
+	Value *big.Int       `json:"value"`
 }
 
 func (b *Transaction) pack() *storage.Transaction {
 	return &storage.Transaction{
-		From:  b.From,
-		Hash:  b.Hash,
+		From:  b.From.String(),
+		Hash:  b.Hash.String(),
 		Input: b.Input,
-		To:    b.To,
-		Value: b.Value,
+		To:    b.To.String(),
+		Value: b.Value.Bytes(),
 	}
 }
 
 func (b *Transaction) unpack(data *storage.Transaction) *Transaction {
-	b.From = data.From
-	b.Hash = data.Hash
+	b.From = common.HexToAddress(data.From)
+	b.Hash = common.HexToHash(data.Hash)
 	b.Input = data.Input
-	b.To = data.To
-	b.Value = data.Value
+	b.To = common.HexToAddress(data.To)
+	b.Value = new(big.Int).SetBytes(data.Value)
 
 	return b
 }
