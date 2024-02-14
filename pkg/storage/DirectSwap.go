b8344e0ccd6e9974593db5dd1fd69d64fabdb096
3hak <al.miroshnychenko@gmail.com>
2024-02-14 13:17:59 +0200

    feat: add ds struct for open search

--- pkg/storage/DirectSwap.go
+++ pkg/storage/DirectSwap.go
@@ -85,3 +85,47 @@ func (ds *DirectSwap) UnMarshal(buf []byte) error {
 	ds.Unpack(data)
 	return nil
 }
+
+type DirectSwapOS struct {
+	Tx          string    `ch:"tx" json:"tx"`
+	Date        time.Time `ch:"date" json:"date"`
+	Chain       string    `ch:"chain" json:"chain"`
+	BlockNumber uint64    `ch:"blocknumber" json:"blocknumber"`
+	Protocol    string    `ch:"protocol" json:"protocol"`
+	SrcToken    string    `ch:"src_token" json:"src_token"`
+	DstToken    string    `ch:"dst_token" json:"dst_token"`
+	Amount0     string    `ch:"amount0" json:"amount0"`
+	Amount1     string    `ch:"amount1" json:"amount1"`
+	PriceA      float32   `ch:"pricea" json:"pricea"`
+	PriceAUSD   float32   `ch:"pricea_usd" json:"pricea_usd"`
+	PriceB      float32   `ch:"priceb" json:"priceb"`
+	PriceBUSD   float32   `ch:"priceb_usd" json:"priceb_usd"`
+	Wallet      string    `ch:"wallet" json:"wallet"`
+	Order       uint16    `ch:"order" json:"order"`
+	ValueUSD    float32   `ch:"value_usd" json:"value_usd"`
+}
+
+func (ds DirectSwapOS) Convert() *DirectSwap {
+
+	amount0, _ := big.NewInt(0).SetString(ds.Amount0, 10)
+	amount1, _ := big.NewInt(0).SetString(ds.Amount1, 10)
+
+	return &DirectSwap{
+		Tx:          ds.Tx,
+		Date:        ds.Date,
+		Chain:       ds.Chain,
+		BlockNumber: ds.BlockNumber,
+		Protocol:    ds.Protocol,
+		SrcToken:    ds.SrcToken,
+		DstToken:    ds.DstToken,
+		Amount0:     amount0,
+		Amount1:     amount1,
+		PriceA:      decimal.NewFromFloat(float64(ds.PriceA)),
+		PriceAUSD:   decimal.NewFromFloat(float64(ds.PriceAUSD)),
+		PriceB:      decimal.NewFromFloat(float64(ds.PriceB)),
+		PriceBUSD:   decimal.NewFromFloat(float64(ds.PriceBUSD)),
+		Wallet:      ds.Wallet,
+		Order:       ds.Order,
+		ValueUSD:    decimal.NewFromFloat(float64(ds.ValueUSD)),
+	}
+}
